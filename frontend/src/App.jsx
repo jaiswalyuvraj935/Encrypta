b308b171e3d330e5d47ab55dfeb3dbea21b8cecf
yuvraj <jaiswalyuvraj935@gmail.com>
2026-08-22 18:51:29 +0530

    f

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,9 +9,10 @@ import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
 import SettingsPage from "./pages/SettingsPage";
 import { useAuthStore } from './store/useAuthStore';
-
+import { useThemeStore } from './store/useThemeStore';
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const { theme } = useThemeStore();
 
   useEffect(() => {
     checkAuth();
@@ -26,7 +27,7 @@ const App = () => {
   }
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
 
       <Routes>
