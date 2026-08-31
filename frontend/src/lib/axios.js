4df0d75af679fdca17b7bdf0f733db0c5b2aa9ec
yuvraj <jaiswalyuvraj935@gmail.com>
2026-08-31 13:25:11 +0530

    f

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
-import axios from "axios"
+import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5001/api",
-    withCredentials: true,
-})
+  baseURL: "https://encrypta.onrender.com/api", // Added /api
+  withCredentials: true,
+});
